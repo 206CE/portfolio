508bc09a7c9069572374c54a3720ddf73543b9e9
JohannesBotha <jacobotha206@gmail.com>
2026-01-08 16:00:09 +0200

    Trying SSG

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* OPTIONS FOR SSG */
-  basePath: "/portfolio",
   output: "export",
-  reactStrictMode: true,
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+
 };
 
 export default nextConfig;
